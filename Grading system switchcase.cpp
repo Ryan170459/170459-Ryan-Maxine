4f8c8561fb395ccb1e88821a058857dd0c2d81ba
Ryan170459 <146387646+Ryan170459@users.noreply.github.com>
2023-11-06 19:14:04 +0300

    vhbh

--- Grading system switchcase.cpp
+++ Grading system switchcase.cpp
@@ -3,47 +3,55 @@
 using namespace std;
 int main()
 {
-    int opt;//switch case variable
-    cout << "Hi, This is your BBIT Sem 1(Sept-Dec 2023)";
-    cout <<"\n1.Mon\n2.Tue\n3.Wed\n4.Thur\n5.Fri" <<endl;
-    cout << "Please select day of choice (i.e. 1, 2, 3, e.t.c.....)" <<endl;
-    cin >> opt;
+    int score;//switch case variable
+    char grade;
+    string fullname,course;
+    cout << "Enter fullname"<<endl;
+    getline(cin,fullname);
+    cout <<"Enter course"<<endl;
+    getline(cin,course);
+    cout << "Enter score"<<endl;
+    cin >> score;
+    
+    
 
-    switch (opt){
-        case 1:
-        cout << "Mon classes\n......" <<endl;
-        cout << "\t1.BBIT 1106: Math for Biz"<<endl;
-        cout << "\t2.BBIT 1102: Intro to programming"<<endl;
+    switch (score/10){
+        case 8:
+        grade ='A';
         break;
 
-        case 2:
-        cout << "Tue classes\n......" <<endl;
-        cout << "\t2.BBIT 1102: Intro to programming"<<endl;
-        cout << "\t1.BBIT 1104: Ethics"<<endl;
+        case 7:
+        grade = 'A';
         break;
 
-        case 3:
-        cout << "Wed classes\n......" <<endl;
-        cout << "\t1.BBIT 1106: Maths for business"<<endl;
-        cout << "\t2.BBIT 1102: Theology"<<endl;
-        cout << "\t3. BBIT 1102: Funamentals of IT"<<endl;
-        break;
-
-        case 4:
-        cout << "Thur classes\n......" <<endl;
-        cout << "\t1.BBIT 1105: Accounting"<<endl;
-        cout << "\t3.BBIT 1101: Fundamentals of IT"<<endl;
+        case 6:
+        grade = 'B';
         break;
 
         case 5:
-        cout << "Fri classes\n......"<<endl;
-        cout << "\t1.BBIT 1103: Business Computing"<<endl;
+        grade = 'C';
+        break;
+
+        case 4:
+        grade = 'E';
+        break;
+
+        case 3:
+        grade = 'F';
         break;
 
         default:
-        cout << "Invalid Input";
+        cout << "Invalid Score!ERROR!";
 
+    
     }
+    
+ cout<< "Student name is "<<fullname<<endl;
+ cout<< "Taking course " <<course<<endl;
+ cout<< "Score is "<<score<<endl;
+ cout<< "Grade is "<<grade<<endl;
+
+ return 0;
 }
     
 
@@ -60,4 +68,4 @@ int main()
 
 
 
-}
+
